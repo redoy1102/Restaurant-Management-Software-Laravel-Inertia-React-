c9c4ab9ddbb93e4637c9d0e5593f933ad71036af
redoy1102 <hridoyvlog9@gmail.com>
2025-08-28 15:11:07 +0600

    [FIT]: All the tables are ready now

---
+++ database/migrations/2025_08_28_052700_create_invoices_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->constrained('orders');
+            $table->foreignId('table_id')->constrained('tables');
+            $table->decimal('total_amount', 10, 2);
+            $table->integer('preparation_time');
+            $table->enum('status', ['paid', 'unpaid'])->default('unpaid');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
